e3582e9140c1e321a897652aadd5aa5133091ed7
KOSASIH <kosasihg88@gmail.com>
2024-06-17 20:59:10 +0700

    Create hyperledger.py

---
+++ blockchain-network-interface/hyperledger/hyperledger.py
@@ -0,0 +1,18 @@
+# hyperledger.py
+import json
+
+class Hyperledger:
+    def __init__(self, config):
+        self.config = config
+
+    def deploy_chaincode(self, chaincode):
+        # TO DO: implement chaincode deployment logic
+        return None
+
+    def invoke_chaincode(self, chaincode, args):
+        # TO DO: implement chaincode invocation logic
+        return None
+
+    def query_chaincode(self, chaincode, args):
+        # TO DO: implement chaincode query logic
+        return None
