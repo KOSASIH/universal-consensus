df617cf6d5543fd24f707fcfdff82a8f4d7c6515
KOSASIH <kosasihg88@gmail.com>
2024-06-23 09:20:35 +0700

    Update main.go

--- blockchain-network-interface/OmniaChain/main.go
+++ blockchain-network-interface/OmniaChain/main.go
@@ -4,55 +4,56 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/KOSASIH/universal-consensus/blockchain-network-interface/OmniaChain"
+	"github.com/KOSASIH/universal-consensus/blockchain-network-interface/OmniaChain/block"
+	"github.com/KOSASIH/universal-consensus/blockchain-network-interface/OmniaChain/blockchain"
 )
 
 func main() {
-	// Initialize the OmniaChain system
-	oc, err := OmniaChain.NewOmniaChain()
-	if err != nil {
-		log.Fatal(err)
+	// Create a new blockchain
+	bc := blockchain.NewBlockchain()
+
+	// Create a new block
+	tx1 := block.Transaction{
+		ID:        "tx1",
+		Timestamp: time.Now(),
+		Sender:    "Alice",
+		Recipient: "Bob",
+		Amount:    10,
 	}
-
-	// Start the OmniaChain system
-	err = oc.Start()
-	if err != nil {
-		log.Fatal(err)
+	tx2 := block.Transaction{
+		ID:        "tx2",
+		Timestamp: time.Now(),
+		Sender:    "Bob",
+		Recipient: "Charlie",
+		Amount:    20,
 	}
-
-	fmt.Println("OmniaChain system started")
-
-	// Create a new transaction
-	tx, err := OmniaChain.NewTransaction("Alice", "Bob", big.NewInt(10), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Add the transaction to the transaction pool
-	err = oc.GetTxPool().AddTx(tx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Transaction added to pool")
-
-	// Mine a new block
-	block, err := oc.GetChain().MineBlock(oc.GetTxPool().GetTxs())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("New block mined:", block.Header.Hash)
+	block := block.NewBlock([]block.Transaction{tx1, tx2}, "")
 
 	// Add the block to the blockchain
-	err = oc.GetChain().AddBlock(block)
-	if err != nil {
+	err := bc.AddBlock(block)
+	if err!= nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Block added to blockchain")
-
-	// Print the current blockchain state
 	fmt.Println("Blockchain state:")
-	fmt.Println(oc.GetChain().String())
+	fmt.Println(bc.GetChain())
+
+	// Mine a new block
+	tx3 := block.Transaction{
+		ID:        "tx3",
+		Timestamp: time.Now(),
+		Sender:    "Charlie",
+		Recipient: "Alice",
+		Amount:    30,
+	}
+	newBlock := block.NewBlock([]block.Transaction{tx3}, bc.GetCurrentBlock().Header.Hash)
+
+	// Add the new block to the blockchain
+	err = bc.AddBlock(newBlock)
+	if err!= nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Updated blockchain state:")
+	fmt.Println(bc.GetChain())
 }
