fead8f68c3f07cd812ef63347f396a01c88cdefd
KOSASIH <kosasihg88@gmail.com>
2024-06-24 15:07:36 +0700

    Create lattice.go

---
+++ crypto/lattice.go
@@ -0,0 +1,11 @@
+// crypto/lattice.go
+package crypto
+
+import (
+	"math/big"
+	"github.com/tuneinsight/lattigo/v3"
+)
+
+func GenerateLatticeKey() (*lattigo.Ring, *lattigo.SecretKey) {
+	// Implement lattice-based cryptography for key generation
+}
