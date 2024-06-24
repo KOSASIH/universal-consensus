f7836df44f556259ff48ed78fc97d99cfec2a482
KOSASIH <kosasihg88@gmail.com>
2024-06-24 15:14:34 +0700

    Create kubernetes.kt

---
+++ cloud/kubernetes.kt
@@ -0,0 +1,9 @@
+// cloud/kubernetes.kt
+package cloud
+
+import io.fabric8.kubernetes.api.model.Pod
+import io.fabric8.kubernetes.client.DefaultKubernetesClient
+
+fun deployPod(pod: Pod) {
+    // Deploy a Kubernetes pod
+}
