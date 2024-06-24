4d2dd683e4c9d044e558e59ab22dce705b1599d1
KOSASIH <kosasihg88@gmail.com>
2024-06-24 15:15:41 +0700

    Create aws.go

---
+++ cloud/aws.go
@@ -0,0 +1,12 @@
+// cloud/aws.go
+package cloud
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func CreateInstance(instanceType string) error {
+	// Create an AWS EC2 instance
+}
