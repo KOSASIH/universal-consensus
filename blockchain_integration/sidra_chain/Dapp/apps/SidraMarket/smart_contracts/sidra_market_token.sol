a89230f28513c57f391cb6ad3243b63f1965ff76
KOSASIH <kosasihg88@gmail.com>
2024-07-28 18:05:29 +0700

    Update sidra_market_token.sol

--- blockchain_integration/sidra_chain/Dapp/apps/SidraMarket/smart_contracts/sidra_market_token.sol
+++ blockchain_integration/sidra_chain/Dapp/apps/SidraMarket/smart_contracts/sidra_market_token.sol
@@ -1,1 +1,24 @@
 
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/SafeERC721.sol";
+
+contract SidraMarketToken {
+    address private owner;
+    mapping (address => uint256) public balances;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        require(msg.sender == owner, "Only the owner can mint tokens");
+        balances[_to] += _amount;
+    }
+
+    function transfer(address _from, address _to, uint256 _amount) public {
+        require(balances[_from] >= _amount, "Insufficient balance");
+        balances[_from] -= _amount;
+        balances[_to] += _amount;
+    }
+}
